a770360da340e0a198486a18bd9741c400fea7df
nadine-shi <nadinshi@gmail.com>
2020-04-10 09:38:24 +0300

    Change text on the login screen

--- js/views/LoginView.js
+++ js/views/LoginView.js
@@ -66,6 +66,14 @@ function CLoginView()
 	this.currentLanguage = ko.observable(UserSettings.Language);
 	this.bAllowChangeLanguage = Settings.AllowChangeLanguage && !App.isMobile();
 	this.bUseDropdownLanguagesView = Settings.UseDropdownLanguagesView;
+	this.headingSelectLanguage = ko.computed(function () {
+		var sSiteName = UserSettings.SiteName;
+		if (_.isEmpty(sSiteName))
+		{
+			sSiteName = TextUtils.i18n('%MODULENAME%/HEADING_DEFAULT_SITENAME');
+		}
+		return TextUtils.i18n('%MODULENAME%/HEADING_SELECT_LANGUAGE', {'SITENAME': sSiteName});
+	}, this);
 	
 	this.domains = ko.observableArray([]);
 	this.selectedDomain = ko.observable('');
