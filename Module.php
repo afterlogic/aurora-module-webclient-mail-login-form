58e6d5172624a5528a1da919b277bbaa7367c6c7
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:57 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -20,107 +20,97 @@ use Aurora\Modules\Mail\Module as MailModule;
  */
 class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
-	/***** public functions might be called with web API *****/
-	/**
-	 * Obtains list of module settings for authenticated user.
-	 * 
-	 * @return array
-	 */
-	public function GetSettings()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
+    /***** public functions might be called with web API *****/
+    /**
+     * Obtains list of module settings for authenticated user.
+     *
+     * @return array
+     */
+    public function GetSettings()
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
 
-		return array(
-			'ServerModuleName' => $this->getConfig('ServerModuleName', ''),
-			'HashModuleName' => $this->getConfig('HashModuleName', ''),
-			'CustomLoginUrl' => $this->getConfig('CustomLoginUrl', ''),
-			'DemoLogin' => $this->getConfig('DemoLogin', ''),
-			'DemoPassword' => $this->getConfig('DemoPassword', ''),
-			'InfoText' => $this->getConfig('InfoText', ''),
-			'BottomInfoHtmlText' => $this->getConfig('BottomInfoHtmlText', ''),
-			'LoginSignMeType' => $this->getConfig('LoginSignMeType', 0),
-			'AllowChangeLanguage' => $this->getConfig('AllowChangeLanguage', true),
-			'UseDropdownLanguagesView' => $this->getConfig('UseDropdownLanguagesView', false),
-		);
-	}
+        return array(
+            'ServerModuleName' => $this->getConfig('ServerModuleName', ''),
+            'HashModuleName' => $this->getConfig('HashModuleName', ''),
+            'CustomLoginUrl' => $this->getConfig('CustomLoginUrl', ''),
+            'DemoLogin' => $this->getConfig('DemoLogin', ''),
+            'DemoPassword' => $this->getConfig('DemoPassword', ''),
+            'InfoText' => $this->getConfig('InfoText', ''),
+            'BottomInfoHtmlText' => $this->getConfig('BottomInfoHtmlText', ''),
+            'LoginSignMeType' => $this->getConfig('LoginSignMeType', 0),
+            'AllowChangeLanguage' => $this->getConfig('AllowChangeLanguage', true),
+            'UseDropdownLanguagesView' => $this->getConfig('UseDropdownLanguagesView', false),
+        );
+    }
 
-	public function Login($Login, $Password, $Domain, $Language = '', $SignMe = false)
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
+    public function Login($Login, $Password, $Domain, $Language = '', $SignMe = false)
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
 
-		$sLogin = \trim($Login);
-		$sDomain = \trim($Domain);
+        $sLogin = \trim($Login);
+        $sDomain = \trim($Domain);
 
-		if (empty($sLogin) || empty($sDomain))
-		{
-			throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::InvalidInputParameter);
-		}
+        if (empty($sLogin) || empty($sDomain)) {
+            throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Notifications::InvalidInputParameter);
+        }
 
-		return \Aurora\Modules\StandardLoginFormWebclient\Module::Decorator()->Login($sLogin . '@' . $sDomain, $Password, $Language, $SignMe);
-	}
+        return \Aurora\Modules\StandardLoginFormWebclient\Module::Decorator()->Login($sLogin . '@' . $sDomain, $Password, $Language, $SignMe);
+    }
 
-	public function GetMailDomains()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
+    public function GetMailDomains()
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
 
-		$bPrevState = \Aurora\System\Api::skipCheckUserRole(true);
-		$aAllDomains = [];
-		$oTenant = \Aurora\System\Api::getTenantByWebDomain();
-		$aArgs = [];
-		if ($oTenant instanceof \Aurora\Modules\Core\Models\Tenant)
-		{
-			$aArgs = [
-				'TenantId' => $oTenant->Id
-			];
-		}
-		$mResult = [];
-		$this->broadcastEvent(
-			'GetMailDomains',
-			$aArgs,
-			$mResult
-		);
-		if (is_array($mResult) && !empty($mResult))
-		{
-			$aAllDomains = $mResult;
-		}
-		else
-		{
-			$Filter = null;
-			if ($oTenant instanceof \Aurora\Modules\Core\Models\Tenant)
-			{
-				$Filter = Server::orWhere(function($query) use ($oTenant) {
-					$query->where('OwnerType', ServerOwnerType::SuperAdmin)
-						->where(function($query) use ($oTenant) {
-							$query->where('TenantId', $oTenant->Id)
-								->where('OwnerType', ServerOwnerType::Tenant);
-						});
-				});
-			}
-			else
-			{
-				//get all servers for all tenants
-				$Filter = Server::where('OwnerType', ServerOwnerType::Tenant)
-					->orWhere('OwnerType', ServerOwnerType::SuperAdmin);
-			}
+        $bPrevState = \Aurora\System\Api::skipCheckUserRole(true);
+        $aAllDomains = [];
+        $oTenant = \Aurora\System\Api::getTenantByWebDomain();
+        $aArgs = [];
+        if ($oTenant instanceof \Aurora\Modules\Core\Models\Tenant) {
+            $aArgs = [
+                'TenantId' => $oTenant->Id
+            ];
+        }
+        $mResult = [];
+        $this->broadcastEvent(
+            'GetMailDomains',
+            $aArgs,
+            $mResult
+        );
+        if (is_array($mResult) && !empty($mResult)) {
+            $aAllDomains = $mResult;
+        } else {
+            $Filter = null;
+            if ($oTenant instanceof \Aurora\Modules\Core\Models\Tenant) {
+                $Filter = Server::orWhere(function ($query) use ($oTenant) {
+                    $query->where('OwnerType', ServerOwnerType::SuperAdmin)
+                        ->where(function ($query) use ($oTenant) {
+                            $query->where('TenantId', $oTenant->Id)
+                                ->where('OwnerType', ServerOwnerType::Tenant);
+                        });
+                });
+            } else {
+                //get all servers for all tenants
+                $Filter = Server::where('OwnerType', ServerOwnerType::Tenant)
+                    ->orWhere('OwnerType', ServerOwnerType::SuperAdmin);
+            }
 
-			$aServers = MailModule::getInstance()->getServersManager()->getServerListByFilter($Filter);
-			if ($aServers)
-			{
-				foreach ($aServers as $oServer)
-				{
-					$aDomains = explode("\n", $oServer->Domains);
-					$aDomains = array_filter($aDomains, function($sDomain) {
-						return $sDomain !== '*';
-					});
-					if (count($aDomains) > 0) {
-						$aAllDomains = array_merge($aAllDomains, $aDomains);
-					}
-				}
-			}
-		}
-		\Aurora\System\Api::skipCheckUserRole($bPrevState);
+            $aServers = MailModule::getInstance()->getServersManager()->getServerListByFilter($Filter);
+            if ($aServers) {
+                foreach ($aServers as $oServer) {
+                    $aDomains = explode("\n", $oServer->Domains);
+                    $aDomains = array_filter($aDomains, function ($sDomain) {
+                        return $sDomain !== '*';
+                    });
+                    if (count($aDomains) > 0) {
+                        $aAllDomains = array_merge($aAllDomains, $aDomains);
+                    }
+                }
+            }
+        }
+        \Aurora\System\Api::skipCheckUserRole($bPrevState);
 
-		return array_unique($aAllDomains);
-	}
-	/***** public functions might be called with web API *****/
+        return array_unique($aAllDomains);
+    }
+    /***** public functions might be called with web API *****/
 }
